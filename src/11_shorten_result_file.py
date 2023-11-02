35a9b985c05754810e1ded00bdb06b8cbad36f8e
Ting-Yu Dai <tudai@login1.ls6.tacc.utexas.edu>
2023-11-02 13:36:45 -0500

    update result file

--- src/11_shorten_result_file.py
+++ src/11_shorten_result_file.py
@@ -1,8 +1,17 @@
 from utils.misc import *
+from pathlib import Path
 
-def shorten_result_file(file_path):
+def shorten_result_file(file_path, random_arg, cli_arg):
     df = read_result_file(file_path)
-    df.to_csv('test.csv')
+    n_export_dir = Path(f'/work/08388/tudai/ls6/citySimOutput/export_csv/{random_arg}')
+    n_export_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(n_export_dir / f'{cli_arg}.csv')
 
 if __name__ == '__main__':
-    shorten_result_file('data/random_urban/random_1/Bogota_El-dorado-hour/Bogota_El-dorado-hour_TH.out')
+    export_dir = Path('/work/08388/tudai/ls6/citySimOutput/export/')
+    file_list = export_dir.glob('random_*/**/*_TH.out')
+    file_list = [x for x in file_list if x.is_file()]
+    s_file = file_list[0]
+    for f in file_list:
+        parts = f.parts
+        shorten_result_file(f, parts[-3], parts[-2])
